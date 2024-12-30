1173b4367552a26f6307a884ab8b22a69018d642
thuydotm <thuydtm.csk9@gmail.com>
2024-12-31 00:46:34 +0700

    ui tests for sliders

--- tests/ui/widgets/test_sliders.py
+++ tests/ui/widgets/test_sliders.py
@@ -2,10 +2,10 @@ import pytest
 
 pytest.importorskip('playwright')
 
-from panel_material_ui.widgets import IntSlider
+from panel_material_ui.widgets import IntSlider, Rating
 
 from playwright.sync_api import expect
-from tests.util import serve_component, wait_until
+from tests.util import serve_component
 
 pytestmark = pytest.mark.ui
 
@@ -32,3 +32,25 @@ def test_int_slider_format(page, color, orientation, track):
     if orientation == "vertical":
         bar_orientation = page.locator(f'.MuiSlider-{orientation}')
         expect(bar_orientation).to_have_count(1)
+
+
+def test_int_slider_value_update(page):
+    widget = IntSlider(value=5, start=0, end=10, step=1)
+    serve_component(page, widget)
+    slider_value = page.locator('.MuiTypography-root')
+
+    for i in range(widget.start, widget.end, widget.step):
+        widget.value = i
+        assert slider_value.inner_text() == str(widget.value)
+
+
+@pytest.mark.parametrize('size', ["small", "medium", "large"])
+def test_rating(page, size):
+    widget = Rating(value=3, size=size)
+    serve_component(page, widget)
+
+    rating = page.locator('.rating')
+    expect(rating).to_have_count(1)
+
+    rating_size = page.locator(f'.MuiRating-size{size.capitalize()}')
+    expect(rating_size).to_have_count(1)
