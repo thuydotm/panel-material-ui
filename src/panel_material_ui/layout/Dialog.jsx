eb7585804dc4980f992df2e6e25ff3920c1531f1
Philipp Rudiger <prudiger@anaconda.com>
2025-01-03 15:17:36 +0100

    Fix lint

--- src/panel_material_ui/layout/Dialog.jsx
+++ src/panel_material_ui/layout/Dialog.jsx
@@ -1,14 +1,14 @@
 import Dialog from "@mui/material/Dialog";
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
 
-export function render({ model, view }) {
+export function render({model, view}) {
   const [full_screen] = model.useState("full_screen");
   const [open] = model.useState("open");
   const [title] = model.useState("title");
   const objects = model.get_child("objects");
 
-  model.on('after_render', () => {
+  model.on("after_render", () => {
     document.head.append(...view.style_cache.children)
   })
 
