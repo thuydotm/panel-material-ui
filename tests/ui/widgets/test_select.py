2ff81eac0dd7b9b14e1dc919be70993746c4b1fd
thuydotm <thuydtm.csk9@gmail.com>
2025-01-03 09:21:31 +0700

    select

--- tests/ui/widgets/test_select.py
+++ tests/ui/widgets/test_select.py
@@ -2,7 +2,7 @@ import pytest
 
 pytest.importorskip('playwright')
 
-from panel_material_ui.widgets import AutocompleteInput
+from panel_material_ui.widgets import AutocompleteInput, Select
 
 from playwright.sync_api import expect
 from tests.util import serve_component, wait_until
@@ -12,9 +12,19 @@ pytestmark = pytest.mark.ui
 
 @pytest.mark.parametrize('variant', ["filled", "outlined", "standard"])
 def test_autocomplete_input_format(page, variant):
-    widget = AutocompleteInput(name='Autocomplete Input test', variant=variant, options=["Option 1", "Option 2", "AB"])
+    widget = AutocompleteInput(name='Autocomplete Input test', variant=variant, options=["Option 1", "Option 2", "123"])
     serve_component(page, widget)
     ai = page.locator(".autocomplete-input")
     wait_until(lambda: expect(ai).to_have_count(1), page=page, timeout=20000)
     ai_format = page.locator(f"div[variant='{variant}']")
     wait_until(lambda: expect(ai_format).to_have_count(1), page=page, timeout=20000)
+
+
+@pytest.mark.parametrize('variant', ["filled", "outlined", "standard"])
+def test_select_format(page, variant):
+    widget = Select(name='Select test', variant=variant, options=["Option 1", "Option 2", "Option 3"])
+    serve_component(page, widget)
+    select = page.locator(".select")
+    wait_until(lambda: expect(select).to_have_count(1), page=page)
+    select_format = page.locator(f".MuiSelect-{variant}")
+    expect(select_format).to_have_count(1)
