8b1de060ad37ea5016fcb2a916980078eaabe427
thuydotm <thuydtm.csk9@gmail.com>
2025-01-12 23:38:50 +0700

    indicators: update docs

--- src/panel_material_ui/widgets/indicators.py
+++ src/panel_material_ui/widgets/indicators.py
@@ -5,6 +5,17 @@ from .base import MaterialWidget
 
 
 class LoadingIndicator(MaterialWidget):
+    """
+    The `LoadingIndicator` provides a visual representation as a spinner of the loading status.
+
+    Reference to the corresponding Panel component `panel.indicators.LoadingSpinner`:
+    https://panel.holoviz.org/reference/indicators/LoadingSpinner.html
+
+    :Example:
+
+    >>> LoadingIndicator(color='success')
+    """
+
     color = param.Selector(objects=COLORS, default="primary")
 
     size = param.Integer(default=None, bounds=(0, None))
@@ -23,6 +34,19 @@ class LoadingIndicator(MaterialWidget):
 
 
 class Progress(MaterialWidget):
+    """
+    The `Progress` widget displays the progress towards some target
+    based on the current `value` and the `max` value.
+
+    Missing features when comparing with the corresponding
+    panel Progress indicator [panel.indicator.Progress](https://panel.holoviz.org/reference/indicators/Progress.html):
+    `active`, `bar_color`, `max`.
+
+    :Example:
+
+    >>> Progress(value=20, color="primary")
+    """
+
     color = param.Selector(objects=COLORS, default="primary")
 
     value = param.Number(default=0, bounds=(0, 100))
